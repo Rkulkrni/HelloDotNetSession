e1783d56ab4b2e0366e54689c28803c8df6c5df3
Rkulkrni <rahulspk7@gmail.com>
2026-02-17 11:15:16 +0530

    OOps Encapsulation

---
+++ OOP's/Encapsulation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloDotNetSession.OOP_s
+{
+    internal class Car
+    {
+        // ** Without encapsulation example
+        //public int speed;
+
+        //public void CarMethod()
+        //{
+        //    Console.WriteLine($"speed = {speed}");
+        //}
+
+        // ** With encapsiualtion example.
+
+        private int Speed;
+
+        public void SetCarSpeedLimit(int speed)
+        {
+
+            if (speed > 0 && speed < 200)
+            {
+                Console.WriteLine($"Speed is : {speed}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid speed");
+            }
+        }
+
+        public int GetCarSpeedLimit()
+        {
+            {
+                return Speed;
+
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using HelloDotNetSession;
+using HelloDotNetSession.OOP_s;
 namespace HelloDotNetSession
 {
     class cSharpBasic
@@ -8,9 +9,34 @@ namespace HelloDotNetSession
 
         public static void Main()
         {
-            LINQ lINQ = new LINQ();
-            lINQ.getLINQOperator();
+            //Encapsulation
 
+            Car car = new Car();
+            car.SetCarSpeedLimit(-100);
+            car.GetCarSpeedLimit();
+
+
+            //Car car = new Car();
+            //car.speed = -100;
+            //car.CarMethod();
+
+            // Class, Object And constructor
+            //Country country = new Country("India", 91);
+            //Country country12 = new Country();
+            //Country Country2 = new Country(country, "Asia");
+
+            //Employee employee = new Employee();
+            //employee.EmpName = "Alex";
+            //employee.EmployeeID = 100;
+            //employee.EmployeeMethod();
+
+            //Employee employee2 = new Employee();
+            //employee2.EmpName = "John";
+            //employee2.EmployeeID = 200;
+            //employee2.EmployeeMethod();
+
+            //LINQ lINQ = new LINQ();
+            //lINQ.getLINQOperator();
 
             //Collections collections = new Collections();
             //collections.getArray();
