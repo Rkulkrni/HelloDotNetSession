2b1fc224bd279bd4068acbb578c9a49a4ebdb4d5
Rkulkrni <rahulspk7@gmail.com>
2026-02-17 11:14:14 +0530

    OOPS : Class and Object

---
+++ OOP's/ObjectAndClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloDotNetSession.OOP_s
+{
+    internal class Employee
+    {
+        // Object : 
+        public string EmpName;
+        public int EmployeeID;
+        public void EmployeeMethod()
+        {
+            Console.WriteLine($"This is employee method: {EmpName} & {EmployeeID}");
+        }
+    }
+}
